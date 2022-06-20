b1f1a0bae71d8f3b4d2268130cffbb1e38de942b
imckify <rokas.mockevicius@gmail.com>
2022-06-20 18:29:21 +0300

    amt BigDecimal

--- backend/src/main/java/com/lbapp/LBcalc/models/CcyAmt.java
+++ backend/src/main/java/com/lbapp/LBcalc/models/CcyAmt.java
@@ -3,10 +3,12 @@ package com.lbapp.LBcalc.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.ToString;
 
+import java.math.BigDecimal;
+
 @ToString
 public class CcyAmt {
     @JsonProperty("Ccy")
     private String currency;
     @JsonProperty("Amt")
-    private double amount;
+    private BigDecimal amount;
 }

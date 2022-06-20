ece160a968479adf75da2bcc9b49f24075cb355d
imckify <rokas.mockevicius@gmail.com>
2022-06-20 16:09:00 +0300

    spring boot init

---
+++ backend/src/main/java/com/lbapp/LBcalc/LBcalc.java
@@ -0,0 +1,13 @@
+package com.lbapp.LBcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LBcalc {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LBcalc.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lbapp/LBcalc/LBcalcTests.java
@@ -0,0 +1,13 @@
+package com.lbapp.LBcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LBcalcTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

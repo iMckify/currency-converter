2cc4c63defe497fca482b9c38e2d80c06e2ffd75
imckify <rokas.mockevicius@gmail.com>
2022-06-21 22:42:38 +0300

    FE. Autocomplete fail

--- GUI/src/components/history/HistoryForm.js
+++ GUI/src/components/history/HistoryForm.js
@@ -3,13 +3,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Button from '@mui/material/Button'
 import ShowChartIcon from '@mui/icons-material/ShowChart'
+import TextField from '@mui/material/TextField'
+import Autocomplete from '@mui/material/Autocomplete'
 import DateBasic from '../common/DateBasic'
+import CrudApi from "../../api/CrudApi";
 
 
 export class HistoryForm extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = { dateFrom: new Date(), dateTo: new Date() }
+		this.state = { dateFrom: new Date(), dateTo: new Date(), symbols: [], symbol: 'EURUSD' }
+	}
+
+	componentDidMount() {
+		CrudApi.getAll('Forex/current')
+			.then((entities) => {
+				this.setState({ symbols: entities })
+				return entities
+			})
+			.catch((error) => {
+				throw error
+			})
 	}
 
 	handleSave = (e) => {
@@ -26,13 +40,68 @@ export class HistoryForm extends React.Component {
 	}
 
 	render() {
-		const { dateFrom, dateTo } = this.state
+		const { dateFrom, dateTo, symbols, symbol } = this.state
 		const header = 'History'
+
+		const optionKey = 'symbol'
 
 		return (
 			<div className='container'>
 				<h1>{header}</h1>
 				<form onSubmit={this.handleSave}>
+					<div className='form-group'>
+						<label>Company</label>
+						<Autocomplete
+							autoSelect
+							value={symbol}
+							loading={symbols.length === 0}
+							options={symbols}
+							getOptionLabel={(option) => {
+								if (option !== '') {
+									return `${option[optionKey]} | ${option.value}`
+								}
+								return ''
+							}}
+							renderOption={(props, option) => (
+								<li {...props} style={{ justifyContent: 'space-between' }}>
+									<div className='mr-5'>{option[optionKey]}</div>
+									<div style={{ fontSize: 11, color: 'darkgray' }}>
+										{option.value}
+									</div>
+								</li>
+							)}
+							onChange={(e, val) => {
+								this.setState({ symbol: val.symbol })
+							}}
+							isOptionEqualToValue={(option, val) => {
+								if (option[optionKey] || val[optionKey]) {
+									return (
+										option[optionKey] === val[optionKey] ||
+										option[optionKey] === symbol
+									)
+								}
+								return option === val || option === symbol
+							}}
+							style={{ width: 400 }}
+							renderInput={(params) => (
+								<TextField
+									{...params}
+									variant='outlined'
+									InputLabelProps={{ shrink: false }}
+									label={symbol || params.inputProps.value ? ' ' : 'Symbol'}
+									sx={{
+										'& label': {
+											'&.Mui-focused': {
+												visibility: 'hidden',
+											},
+										},
+										marginBottom: `${1}rem`,
+									}}
+								/>
+							)}
+						/>
+					</div>
+
 					<DateBasic
 						name='dateFrom'
 						label='Date From'

a453ccf3ce5a0052e068e084effd7eab4b7e7d38
imckify <rokas.mockevicius@gmail.com>
2022-06-21 22:14:49 +0300

    FE. dateTo, submit button

--- GUI/src/components/history/HistoryForm.js
+++ GUI/src/components/history/HistoryForm.js
@@ -1,16 +1,19 @@
 /* eslint-disable */
 import React from 'react'
 import PropTypes from 'prop-types'
+import Button from '@mui/material/Button'
+import ShowChartIcon from '@mui/icons-material/ShowChart'
 import DateBasic from '../common/DateBasic'
 
 
 export class HistoryForm extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state= { dateFrom: undefined}
+		this.state = { dateFrom: new Date(), dateTo: new Date() }
 	}
 
-	handleSave = (x) => {
+	handleSave = (e) => {
+		const { dateFrom, dateTo } = this.state
 		console.log('handleSave')
 	}
 
@@ -18,8 +21,12 @@ export class HistoryForm extends React.Component {
 		this.setState({ dateFrom: date })
 	}
 
+	handleChangeDateTo = (date) => {
+		this.setState({ dateTo: date })
+	}
+
 	render() {
-		const { dateFrom } = this.state
+		const { dateFrom, dateTo } = this.state
 		const header = 'History'
 
 		return (
@@ -34,6 +41,27 @@ export class HistoryForm extends React.Component {
 						value={dateFrom}
 						change={this.handleChangeDateFrom}
 					/>
+
+					<DateBasic
+						name='dateTo'
+						label='Date To'
+						mode='past'
+						required
+						value={dateTo}
+						change={this.handleChangeDateTo}
+					/>
+
+					<div>
+						<Button
+							type='submit'
+							color='primary'
+							variant='contained'
+							disabled={ dateFrom.getTime() > dateTo.getTime() }
+							startIcon={<ShowChartIcon style={{ marginRight: -5 }} />}
+						>
+							View Prices
+						</Button>
+					</div>
 				</form>
 			</div>
 		)

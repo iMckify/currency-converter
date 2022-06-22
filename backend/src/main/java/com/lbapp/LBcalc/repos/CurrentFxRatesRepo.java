b0de04cd84d95016ac4e39dcc04882b86c5c75fe
imckify <rokas.mockevicius@gmail.com>
2022-06-23 02:25:23 +0300

    BE. Converter BigDecimal explanation and fixes: scale 16, multiplication rounding, exceptions for input and notfound

--- backend/src/main/java/com/lbapp/LBcalc/repos/CurrentFxRatesRepo.java
+++ backend/src/main/java/com/lbapp/LBcalc/repos/CurrentFxRatesRepo.java
@@ -3,6 +3,8 @@ package com.lbapp.LBcalc.repos;
 import com.lbapp.LBcalc.models.CurrentFxRate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CurrentFxRatesRepo extends JpaRepository<CurrentFxRate, Integer> {
-    CurrentFxRate findBySymbolContains (String symbol);
+    Optional<CurrentFxRate> findBySymbolContains (String symbol);
 }
--- backend/src/main/java/com/lbapp/LBcalc/services/ConverterService.java
+++ backend/src/main/java/com/lbapp/LBcalc/services/ConverterService.java
@@ -1,15 +1,13 @@
 package com.lbapp.LBcalc.services;
 
-import com.lbapp.LBcalc.models.CurrentFxRate;
 import com.lbapp.LBcalc.repos.CurrentFxRatesRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.math.RoundingMode;
-import java.util.List;
 
 @Service
 public class ConverterService {
@@ -22,38 +20,42 @@ public class ConverterService {
         this.currentFxRatesRepo = currentFxRatesRepo;
     }
 
-    // https://sdw.ecb.europa.eu/curConverter.do
+    // https://sdw.ecb.europa.eu/curConverter.do working converter
     // 100 USD to AUD
-    // 100 / 1,0517 USD * 1,5061 AUD = 143.20623752
+    // 100 / 1.0521 EUR/USD * 1.5254 EUR/AUD = 140.6366314989069000
     // input / first * second
     public BigDecimal convertAPI(String symbolSelected, String symbolTarget, BigDecimal input) {
-        List<CurrentFxRate> allRates = this.currentFxRatesRepo.findAll();
-
-        BigDecimal firstRate = null;
-        if (symbolSelected.equals("EUR")) {
-            firstRate = BigDecimal.ONE;
-        } else {
-            firstRate = this.currentFxRatesRepo.findBySymbolContains(symbolSelected).getValue();
-        }
-
-        BigDecimal secondRate = null;
-        if (symbolTarget.equals("EUR")) {
-            secondRate = BigDecimal.ONE;
-        } else {
-            secondRate = this.currentFxRatesRepo.findBySymbolContains(symbolTarget).getValue();
-        }
+        BigDecimal firstRate = getPrice(symbolSelected);
+        BigDecimal secondRate = getPrice(symbolTarget);
 
         logger.info("Converting " + symbolSelected + "/" + symbolTarget);
         return convertBigDec(firstRate, secondRate, input);
     }
 
-    public BigDecimal convertBigDec(BigDecimal firstRate, BigDecimal secondRate, BigDecimal amount) {
+    // https://keisan.casio.com/calculator checked with settings: mode Real RAD, digits 18, answer standard, accuracy on
+    // Casino never loses:
+    //  1)  division is FLOOR, so 17th digit is ignored and dropped
+    //  2)  multiplication is DECIMAL64, so 17th digit is ignored and dropped
+    // In conclusion, casino wins amount of 17th digit each time
+    private BigDecimal convertBigDec(BigDecimal firstRate, BigDecimal secondRate, BigDecimal amount) {
         logger.info("firstRate:  " + firstRate);
         logger.info("secondRate: " + secondRate);
-        BigDecimal result = amount.divide(firstRate, 18, RoundingMode.FLOOR).multiply(secondRate);
-        logger.info("Result:\t" + result.toPlainString());
-        BigDecimal resultScaled = result.setScale(18, BigDecimal.ROUND_FLOOR);
+        BigDecimal result = amount.divide(firstRate, 16, RoundingMode.FLOOR).multiply(secondRate, MathContext.DECIMAL64);
+        logger.info("Result:\t\t" + result.toPlainString());
+        BigDecimal resultScaled = result.setScale(16, RoundingMode.FLOOR);
         logger.info("Converted:\t" + resultScaled.toPlainString());
         return resultScaled;
+    }
+
+    private BigDecimal getPrice(String symbol) {
+        BigDecimal bigDecimal = null;
+        if (symbol == null) {
+            throw new IllegalArgumentException("Bad symbol");
+        } else if (symbol.equals("EUR")) {
+            bigDecimal = BigDecimal.ONE;
+        } else {
+            bigDecimal = this.currentFxRatesRepo.findBySymbolContains(symbol).orElseThrow(() -> new IllegalArgumentException("Bad symbol")).getValue();
+        }
+        return bigDecimal;
     }
 }

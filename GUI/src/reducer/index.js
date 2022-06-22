1e42e189914353aa50afedc2ff662247aa0c8587
imckify <rokas.mockevicius@gmail.com>
2022-06-22 21:09:15 +0300

    FE. package.json deps

--- GUI/src/reducer/index.js
+++ GUI/src/reducer/index.js
@@ -1,9 +1,6 @@
 import { combineReducers } from 'redux'
-import { reducer as formReducer } from 'redux-form'
 import selectedReducer from './selectedReducer'
 
 export default combineReducers({
-	form: formReducer,
-
 	selectedReducer,
 })

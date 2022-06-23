b381066862bd4bce2d0aa4b4c598e7422bca0fdd
Rokas Mockevičius <47943619+iMckify@users.noreply.github.com>
2022-06-23 05:03:37 +0300

    href

--- GUI/src/components/common/HomeWindow.js
+++ GUI/src/components/common/HomeWindow.js
@@ -8,6 +8,10 @@ function HomeWindow() {
 					<h1 className='display-4'>Lietuvos Bankas forex exchange</h1>
 					<p className='lead'>by iMckify</p>
 				</div>
+				<h4 className='text-warning'>
+					{/* eslint-disable-next-line */}
+					Please, review my code at <a href="https://github.com/iMckify/Bakalauras">Investment Portfolio Analysis System</a>
+				</h4>
 			</div>
 		</header>
 	)
